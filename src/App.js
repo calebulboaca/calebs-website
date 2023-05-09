8407ba6904bfddf36277f1dd9f2162cf4b0bf9b9
calebulboaca <131691565+calebulboaca@users.noreply.github.com>
2023-05-09 10:40:31 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Caleb's Website</h1>
+      <h2>Start editing to see some magic happen!</h2>
+
+      <p>Jashan is a chubby monkey and Caleb is a giraffe.</p>
+
+      <button className="ClickMe">CLICK ME!</button>
+    </div>
+  );
+}
